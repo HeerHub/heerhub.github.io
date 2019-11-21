3b1a0ef1954cead50371be7781ddf6b9879ad9c3
militariahub <bobsmith1249@gmail.com>
2019-11-21 00:17:22 +0000

    Initial Release
    Little content, primary structure.

---
+++ assets/js/useful_links_js.js
@@ -0,0 +1,22 @@
+	$("#includedContent2").load("b.html", function(){
+	  /// new html exists now
+	  $('#myTable').DataTable(/*options*/)
+	});
+	
+    $("#includedContent").load("b.html", function(){
+	  /// new html exists now
+	  $('#myTable').DataTable(/*options*/)
+	});
+	
+	// Get card element
+	const card = document.querySelectorAll('.card');
+
+	// Loop through cards. 
+	// This is so you can have multiple cards on a page.
+	for (let i = 0; i < card.length; i++) {
+	   // Add a click event listener to each card.
+	   card[i].addEventListener("click", function() {
+		 // Toggle active class on card
+		 card[i].classList.toggle("active");
+	   });
+	}
