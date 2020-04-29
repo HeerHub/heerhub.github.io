92fc5fe5e3d6713589a776f9077c143dac3fde16
HeerHub <32578452+HeerHub@users.noreply.github.com>
2020-04-29 23:33:50 +0100

    Version 2.0
    Design overhaul, bug fixes, code refinement, first item page added.

--- assets/js/useful_links_js.js
+++ assets/js/useful_links_js.js
@@ -20,3 +20,38 @@
 		 card[i].classList.toggle("active");
 	   });
 	}
+	
+	
+
+
+
+	$(document).ready(function(){
+	// Should cache elements here for continuous access
+	const footer = $(".footer");
+	const scrollBtn = $("#back-to-top"); 
+	const padding = 25; // So you can change this is one value (scroll to top button bottom padding)
+		$(window).scroll(function () {
+			
+			// Where we're gonna set the button's height
+			var distanceFromBottom = Math.floor($(document).height() - $(document).scrollTop() - $(window).height());
+			// Check to see if we're within the footer range
+			if ( distanceFromBottom <= footer.height() ) {
+					console.log(distanceFromBottom);
+					scrollBtn.css("bottom", (footer.height() - distanceFromBottom) + padding);
+				} else {
+					scrollBtn.css("bottom", padding);
+				}
+				if ($(this).scrollTop() > 50) { 
+					$('#back-to-top').fadeIn();
+				} else {
+					$('#back-to-top').fadeOut();
+				}
+			});
+			// scroll body to 0px on click
+			$('#back-to-top').click(function () {
+				$('body,html').animate({
+					scrollTop: 0 
+				}, 10);  //set time delay before it scrolls up here
+				return false;
+			});
+	});
